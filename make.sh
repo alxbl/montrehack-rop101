e57b4538ac46a382ce996881d3fd97ce0ffb4848
Alexandre Beaulieu <alex@segfault.me>
2019-02-16 14:25:45 -0500

    chore: added challenge 2 and 3 to makefile.

--- make.sh
+++ make.sh
@@ -1,7 +1,12 @@
 #!/bin/bash
 ### Poor man's Makefile.
+
 # Challenge 1
- gcc -no-pie -fno-stack-protector --static 1.c -o chal1
+ gcc -no-pie -fno-stack-protector --static 1.c -o motd_v0.1
+
  # Challenge 2
- # Challenge 3?
+ gcc -no-pie -fno-stack-protector 2.c -o motd_v0.2
+
+ # Challenge 3
+ gcc -no-pie -fno-stack-protector 3.c -o motd_v0.3
 
